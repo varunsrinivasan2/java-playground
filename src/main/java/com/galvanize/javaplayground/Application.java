75d02a41c3e6bcb7b1df8d697ffdab791d8b55a4
varunsrinivasan2 <varunsrinivasan2@gmail.com>
2022-07-12 12:02:04 -0700

    Name and email formatter

--- src/main/java/com/galvanize/javaplayground/Application.java
+++ src/main/java/com/galvanize/javaplayground/Application.java
@@ -3,7 +3,14 @@ package com.galvanize.javaplayground;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        if (args.length == 0) {
+            System.out.println("Please specify a name and email");
+        } else if (args.length == 1) {
+            System.out.println("Please specify an email for " + args[0]);
+        } else {
+            System.out.println(args[0]);
+            System.out.println("<" + args[1] + ">");
+        }
     }
 
 }
